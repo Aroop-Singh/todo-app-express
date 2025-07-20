a564181eb075dff117fa258b518a8c71506b3628
Aroop Singh <aroopprateeksingh2@gmail.com>
2025-07-20 09:47:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-// server.js
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const serverless = require("serverless-http");
 
-// Middleware to serve static files from "public" folder
 app.use(express.static("public"));
 
-// Optional: handle a GET route
 app.get("/health", (req, res) => {
   res.send("Server is healthy 🚀");
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+module.exports.handler = serverless(app);
