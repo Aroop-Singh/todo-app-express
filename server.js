08488dab846035a327fdf44797b20092cada13c8
Aroop Singh <aroopprateeksingh2@gmail.com>
2025-07-22 14:40:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const serverless = require("serverless-http");
+const path = require("path");
 
 app.use(express.static("public"));
+
+// 👇 Add this route just after the middleware
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.get("/health", (req, res) => {
   res.send("Server is healthy 🚀");
